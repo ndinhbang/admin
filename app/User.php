d86219f0c203c09e17de88ff171b2bc457991964
bangnd <ndinhbang111@gmail.com>
2019-08-29 01:51:09 +0700

    config Passport

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,7 +10,7 @@ use Silber\Bouncer\Database\HasRolesAndAbilities;
 
 class User extends Authenticatable
 {
-    use Notifiable, HasRolesAndAbilities;
+    use HasApiTokens, HasRolesAndAbilities, Notifiable;
 
     /**
      * The attributes that are mass assignable.
