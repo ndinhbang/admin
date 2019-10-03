3444035a5171bd4337882466a85a8d945b7c2564
ndinhbang <ndinhbang111@gmail.com>
2019-10-03 18:09:41 +0700

    fix cross-origin problem

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Silber\Bouncer\BouncerServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
---
+++ config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+   
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['http://localhost:8000'],
+    'allowedOriginsPatterns' => ['/localhost:\d/'],
+    'allowedHeaders' => ['Content-Type', 'X-Requested-With', 'Authorization'],
+    'allowedMethods' => ['GET', 'POST', 'PUT','DELETE', 'OPTIONS'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+
+];
