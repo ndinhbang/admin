f30725d12dc5ce84730fb6a96314d621bac025ca
ndinhbang <ndinhbang111@gmail.com>
2019-10-15 15:32:11 +0700

    encrypt tokens ans save to indexedDb

--- frontend/src/store/modules/auth.js
+++ frontend/src/store/modules/auth.js
@@ -1,9 +1,6 @@
 import Auth from '@/api/auth'
 import db from '@/utils/db'
-// import vault from '@/utils/vault'
-import {keyFromPassphrase, getRandomValues} from '@/utils/webCrypto'
-// import {arrayBufferToHexString} from '@/utils/converters'
-// import {handleException} from '@/utils/helpers'
+import {keyFromPassphrase} from '@/utils/webCrypto'
 
 const state = {
     isAuthenticated: false,
@@ -22,23 +19,19 @@ const mutations = {
 const actions = {
     async login({commit}, credentials) {
         const {data} = await Auth.login(credentials)
-        // create key for encryption
         try {
+            // derive crypto key from passphrase for encrypt/decrypt data later
             const cryptoKey = await keyFromPassphrase(credentials.password)
-
             await db.transaction('rw', db.vaults, db.meta, function () {
                 // parallel runing
                 return Promise.all([
-                    // set cryptoKey to encrypt/decrypt data later
+                    // set cryptoKey to indexedDb
                     db.setCryptoKey(cryptoKey),
                     // save tokens to vaults
                     db.setToken(cryptoKey, {kname: 'access_token', v: data['access_token']}),
                     db.setToken(cryptoKey, {kname: 'refresh_token', v: data['refresh_token']})
                 ])
             })
-            // let record = await db.getToken(cryptoKey,'refresh_token')
-            // if (record.v === data.refresh_token) console.log('yay')
-
             commit('login', true)
         } catch (err) {
             // handleException(err)
