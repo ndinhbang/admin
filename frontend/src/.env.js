e799a1f4d62214cda95a26d783b675c560d83c0e
ndinhbang <ndinhbang111@gmail.com>
2019-10-18 15:07:18 +0700

    clear frontend

--- frontend/src/.env.js
+++
@@ -1,6 +0,0 @@
-export const APP_NAME = `Laravel`
-export const APP_URL = `http://cms.local/api`
-export const APP_TIMEZONE = `Asia/Ho_Chi_Minh`
-export const APP_LOCALE = `vi`
-
-
--- frontend/src/utils/antiDebuger.js
+++
@@ -1,4 +0,0 @@
-/**
- * @link https://x-c3ll.github.io/posts/javascript-antidebugging/?fbclid=IwAR0ll9WGSp8CaTMp8L8CeMMaFj5OkXcVhehdgiNRubtakd6NVkY2Kl8_ir0
- */
-export default {}
