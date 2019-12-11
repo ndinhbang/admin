a66032624bba06342a38feb3e0b6cc9ec52b2432
changmua <cryingbecauseofyou@gmail.com>
2019-12-11 14:23:39 +0700

    fix print template

--- resources/views/print/templates/pos80.blade.php
+++ resources/views/print/templates/pos80.blade.php
@@ -58,11 +58,11 @@
                             <td class="text-left" colspan="4"><strong>@{{incremented @index}}.</strong> @{{ name }}</td>
                         </tr>
                         <tr>
-                            <td class="text-left">@{{ price }}</td>
+                            <td class="text-left">@{{money price}}</td>
                             <td class="text-right">@{{ quantity }}</td>
-                            <td class="text-right">>@{{ discount_amount }}</td>
+                            <td class="text-right">>@{{money discount_amount}}</td>
                             <td class="text-right">
-                                <strong>@{{ total_price }}</strong></td>
+                                <strong>@{{money total_price}}</strong></td>
                         </tr>
                         @{{/each}}
                         <tr>
@@ -71,17 +71,17 @@
                         <tr>
                             <td class="text-left pb-1" colspan="2"><strong>Tổng tiền hàng: </strong></td>
                             <td class="text-right py-0 pb-1" colspan="2">
-                                <strong>@{{ amount }}</strong></td>
+                                <strong>@{{money amount}}</strong></td>
                         </tr>
                         <tr>
                             <td class="text-left py-0 pb-1" colspan="2"><strong>Khách trả: </strong></td>
                             <td class="text-right py-0 pb-1" colspan="2">
-                                <strong>@{{ received_amount }}</strong></td>
+                                <strong>@{{money received_amount}}</strong></td>
                         </tr>
                         <tr>
                             <td class="text-left py-0 pb-1" colspan="2"><strong>Đã thanh toán: </strong></td>
                             <td class="text-right py-0 pb-1" colspan="2">
-                                <strong>@{{ paid }}</strong></td>
+                                <strong>@{{money paid}}</strong></td>
                         </tr>
                         <tr>
                             <td class="text-left py-0 pb-1" colspan="4">Ghi chú:</td>
