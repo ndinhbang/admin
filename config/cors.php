5f54807bb4f0fb04312fdda875e52ed90a000282
changmua <cryingbecauseofyou@gmail.com>
2020-01-07 09:52:01 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
    
     'supportsCredentials' => false,
     'allowedOrigins' => ['http://localhost:8000'],
-    'allowedOriginsPatterns' => ['/localhost:\d/','/[a-z\.]*goido\.local/', '/192\.168\.[0-9\.:]*/'],
+    'allowedOriginsPatterns' => ['/localhost:\d/','/[a-z\.]*goido\.net/','/[a-z\.]*goido\.local/', '/192\.168\.[0-9\.:]*/'],
     'allowedHeaders' => ['Content-Type', 'X-Requested-With', 'Authorization', 'X-Place-Id'],
     'allowedMethods' => ['GET', 'POST', 'PUT','DELETE'],
     'exposedHeaders' => [],
