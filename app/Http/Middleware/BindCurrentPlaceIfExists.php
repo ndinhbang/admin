7c71968abf7e2d4c3938c2c9f1c0d942818613d2
ndinhbang <ndinhbang111@gmail.com>
2020-02-12 15:21:06 +0700

    [fix] lỗi khi giả lập chủ cửa hàng

--- app/Http/Middleware/BindCurrentPlaceIfExists.php
+++ app/Http/Middleware/BindCurrentPlaceIfExists.php
@@ -10,24 +10,23 @@ class BindCurrentPlaceIfExists
     /**
      * throw Exception if X-Place-Id not set
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure                 $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure                  $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         // return error if place is not exists in case of requiring place uuid
-        if (getBindVal('__requirePlace')
-            && (is_null($uuid = $request->header('X-Place-Id'))
-                || is_null($place = Place::where('uuid', $uuid)->first()))) {
+        if ( ( is_null($uuid = $request->header('X-Place-Id'))
+                || is_null($place = Place::where('uuid', $uuid)
+                    ->first()) )
+            && getBindVal('__requirePlace') ) {
             return $request->expectsJson()
-                ? response()->json(['message' => 'Bad request'], 400)
+                ? response()->json([ 'message' => 'Bad request' ], 400)
                 : response('Bad request', 400);
         }
-
         // make current place instance available as a global
         app()->instance('__currentPlace', $place ?? null);
-
         return $next($request);
     }
 }
