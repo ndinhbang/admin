65ae92a825e6c4a3e6abdb35fbaf98a38dd68ed6
ndinhbang <ndinhbang111@gmail.com>
2020-02-14 12:10:13 +0700

    allow admin fullaccess to websocket channel

--- app/Http/Middleware/RequirePlace.php
+++ app/Http/Middleware/RequirePlace.php
@@ -17,54 +17,51 @@ class RequirePlace
         'print/preview*',
         'print/report*',
         '_debugbar/*',
+        'websockets*',
     ];
 
     /**
      * throw Exception if X-Place-Id not set
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure                 $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure                  $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if ($this->inExceptArray($request)) {
+        if ( $this->inExceptArray($request) ) {
             return $next($request);
         }
-
-        if (is_null($uuid = $request->header('X-Place-Id'))) {
+        if ( is_null($uuid = $request->header('X-Place-Id')) ) {
             return $request->expectsJson()
-                ? response()->json(['message' => 'Bad request'], 400)
+                ? response()->json([ 'message' => 'Bad request' ], 400)
                 : response('Bad request', 400);
         }
-
         // mark require place
         app()->instance('__requirePlace', true);
-
         return $next($request);
     }
 
     /**
      * Determine if the request has a URI that should pass through verification.
      *
-     * @param Request $request
+     * @param  Request  $request
      * @return bool
      */
     protected function inExceptArray(Request $request)
     {
-        if (app()->isLocal()) {
+        if ( app()->isLocal() ) {
             $this->except[] = 'telescope*';
+//            $this->except[] = 'test*';
         }
-        foreach ($this->except as $except) {
-            if ($except !== '/') {
+        foreach ( $this->except as $except ) {
+            if ( $except !== '/' ) {
                 $except = trim($except, '/');
             }
-
-            if ($request->fullUrlIs($except) || $request->is($except)) {
+            if ( $request->fullUrlIs($except) || $request->is($except) ) {
                 return true;
             }
         }
-
         return false;
     }
 }
