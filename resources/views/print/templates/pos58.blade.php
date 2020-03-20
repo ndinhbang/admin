16779105a3d68139645fbd9c88f73ecff02146de
ndinhbang <ndinhbang111@gmail.com>
2020-03-20 09:37:01 +0700

    fix #147: Thêm giờ và mã hóa đơn vào tem

--- resources/views/print/templates/pos58.blade.php
+++ resources/views/print/templates/pos58.blade.php
@@ -7,47 +7,66 @@
     </style>
 </head>
 <body class="receipt w50mm">
-        <section class="sheet padding-5mm">
-            <div class="print">
-                <div class="print-header my-0 mb-0">
-                    <table>
-                        <thead>
-                            <tr>
-                                <td class="text-left py-0 " width="93%"><div class="no-wrap"><strong>@{{ card_name }}</strong> | @{{ area_name }}-@{{ table_name }}</div></td>
-                                <td class="text-right py-0"><strong class="no-wrap">@{{ stt }}</strong></td>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-                <div class="order-info">
-                    <table>
-                        <thead>
-                            <tr>
-                                <td class="text-left py-0" width="90%"><strong>@{{ item_name }}</strong></td>
-                                <td class="text-right py-0"><div class="no-wrap">@{{money item_price }}</div></td>
-                            </tr>
-                            @{{#if children}}
-                            <tr>
-                                <td class="text-left py-0" colspan="2">
-                                    <small>
-                                        @{{#each children}}
-                                            - @{{ product_name }};
-                                        @{{/each}}
-                                    </small>
-                                </td>
-                            </tr>
-                            @{{/if}}
-                            @{{#if item_note}}
-                            <tr>
-                                <td class="text-left" colspan="2"><div class="no-wrap"><em>@{{ item_note }}</em></div></td>
-                            </tr>
-                            @{{/if}}
-                        </thead>
-                    </table>
-                </div>
-            </div>
-        </section>
-
-    </body>
-
+<section class="sheet padding-5mm">
+    <div class="print">
+        <div class="print-header my-0 mb-0">
+            <table>
+                <thead>
+                <tr>
+                    <td class="text-left py-0 " width="93%">
+                        <div class="no-wrap"><strong>@{{ card_name }}</strong> | @{{ area_name }}-@{{ table_name }}
+                        </div>
+                    </td>
+                    <td class="text-right py-0"><strong class="no-wrap">@{{ stt }}</strong></td>
+                </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="order-info">
+            <table>
+                <thead>
+                <tr>
+                    <td class="text-left py-0" width="90%"><strong>@{{ item_name }}</strong></td>
+                    <td class="text-right py-0">
+                        <div class="no-wrap">@{{money item_price }}</div>
+                    </td>
+                </tr>
+                @{{#if children}}
+                <tr>
+                    <td class="text-left py-0" colspan="2">
+                        <small>
+                            @{{#each children}}
+                            - @{{ product_name }};
+                            @{{/each}}
+                        </small>
+                    </td>
+                </tr>
+                @{{/if}}
+                @{{#if order_code}}
+                <tr>
+                    <td class="text-left" colspan="2">
+                        <div class="no-wrap"><em>@{{ order_code }}</em></div>
+                    </td>
+                </tr>
+                @{{/if}}
+                @{{#if created_at}}
+                <tr>
+                    <td class="text-left" colspan="2">
+                        <div class="no-wrap"><em>@{{ created_at }}</em></div>
+                    </td>
+                </tr>
+                @{{/if}}
+                @{{#if item_note}}
+                <tr>
+                    <td class="text-left" colspan="2">
+                        <div class="no-wrap"><em>@{{ item_note }}</em></div>
+                    </td>
+                </tr>
+                @{{/if}}
+                </thead>
+            </table>
+        </div>
+    </div>
+</section>
+</body>
 </html>
